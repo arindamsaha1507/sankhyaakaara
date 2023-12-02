77bc203ad99f35c4f8528e57e4f6880d765feb63
Arindam Saha <arindamsaha1507@gmail.com>
2023-12-02 09:44:06 -0500

    Wide app

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 """The app module."""
 
-import clipboard
 import streamlit as st
 import yaml
 
@@ -57,6 +56,8 @@ def writer(content: str, language: str, script: str) -> str:
 
 def main():
     """The main function."""
+
+    st.set_page_config(layout="wide")
 
     converter = Converter()
 
