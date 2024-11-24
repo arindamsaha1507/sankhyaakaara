d2ff9e4836cdb73b4b05662624e059c65e6b1b6b
Arindam Saha <arindamsaha1507@gmail.com>
2024-11-24 12:05:54 +0000

    Added long description and requirements

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
 """Setup file for sankhyaakaara package."""
 
+import pathlib
 from setuptools import setup, find_packages
+
+# Read the contents of the README file
+current_dir = pathlib.Path(__file__).parent
+long_description = (current_dir / "README.md").read_text()
+with open(current_dir / "requirements.txt", encoding="utf-8") as f:
+    install_requires = f.read().splitlines()
+
 
 setup(
     name="sankhyaakaara",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[],
     include_package_data=True,
+    install_requires=install_requires,
+    package_data={
+        "sankhyaakaara": ["data/*"],  # Include all files in the data directory
+    },
     description="A package for Sanskrit numeral processing and sandhi.",
+    long_description=long_description,  # Add long description
+    long_description_content_type="text/markdown",  # Specify content type
     author="Arindam Saha",
     author_email="arindamsaha1507@gmail.com",
     license="GPL-3.0",
