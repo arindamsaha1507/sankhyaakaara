6dc4720837e799ff6fc17901c7bd2fcc6307a922
Arindam Saha <arindamsaha1507@gmail.com>
2024-11-24 11:39:54 +0000

    Added setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""Setup file for sankhyaakaara package."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="sankhyaakaara",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True,
+    description="A package for Sanskrit numeral processing and sandhi.",
+    author="Arindam Saha",
+    author_email="arindamsaha1507@gmail.com",
+    license="GPL-3.0",
+)
